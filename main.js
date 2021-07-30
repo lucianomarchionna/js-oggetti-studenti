0020949e62f433a581fe2a67b4814c6234c29ad5
Luciano <lucco6823@gmail.com>
2021-07-30 15:56:39 +0200

    Creazione studente

---
+++ main.js
@@ -0,0 +1,10 @@
+// Creare un oggetto che descriva uno studente, con le seguenti proprietà: nome, cognome e età.
+var studente = {
+    nome : "Luciano",
+    cognome : "Marchionna",
+    eta : 20
+};
+// Stampare a schermo attraverso un ciclo for-in tutte le proprietà dell'oggetto.
+// Creare un array di oggetti di studenti.
+// Ciclare su tutti gli studenti e stampare per ognuno di essi, nome e cognome.
+// Dare la possibilità all’utente, attraverso 3 prompt(), di aggiungere un nuovo oggetto studente inserendo nell’ordine: nome, cognome e età.
