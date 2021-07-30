d5b58a4c3fb019f421158d6391bcf430665e9c20
Luciano <lucco6823@gmail.com>
2021-07-30 16:05:50 +0200

    Aggiunta ciclo for-in

--- main.js
+++ main.js
@@ -5,6 +5,9 @@ var studente = {
     eta : 20
 };
 // Stampare a schermo attraverso un ciclo for-in tutte le proprietà dell'oggetto.
+for(let key in studente){
+    console.log(key + " : " + studente[key]);
+}
 // Creare un array di oggetti di studenti.
 // Ciclare su tutti gli studenti e stampare per ognuno di essi, nome e cognome.
 // Dare la possibilità all’utente, attraverso 3 prompt(), di aggiungere un nuovo oggetto studente inserendo nell’ordine: nome, cognome e età.
